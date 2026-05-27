f4e891774768fbcb39954dd386a01f052ef4f0b2
Sverre Akersveen <sakersveen@gmail.com>
2026-05-27 22:28:44 +0200

    updated env vars

--- src/lib/track.ts
+++ src/lib/track.ts
@@ -147,6 +147,7 @@ export const Events = {
   RECEIPT_IMPORT_PUBLISHED:  'receipt_import_published',  // { source, item_count, store, categories }
   RECEIPT_IMPORT_FAILED:     'receipt_import_failed',     // { source }
 
+  
 } as const
 
 export type EventName = typeof Events[keyof typeof Events]
